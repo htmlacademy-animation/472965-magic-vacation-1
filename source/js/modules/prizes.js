53759637f2aeb076f5fae0828908078d20198cb3
Victoria <vicczubareva@yandex.ru>
2020-10-31 20:22:58 +0300

    анимация второстепенного приза

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,5 +1,6 @@
 export default () => {
   const prizeJourneys = document.querySelector(`.prizes__item--journeys img`);
+  const prizeCases = document.querySelector(`.prizes__item--cases img`);
 
   document.body.addEventListener(`screenChanged`, (event) => {
     if (event.detail.screenName === `prizes`) {
@@ -7,6 +8,10 @@ export default () => {
       // если нет картинки, то добавляем
       if (!prizeJourneys.hasAttribute(`src`)) {
         prizeJourneys.setAttribute(`src`, `img/prize1.svg`);
+
+        setTimeout(() => {
+          prizeCases.setAttribute(`src`, `img/prize2.svg`);
+        }, 4000);
       }
     }
   });
