29f469f24a595f22de628e35ecb3afb084f2856e
Keks <keks@htmlacademy.ru>
2020-11-07 09:51:51 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/script.js
+++ 8/js/script.js
@@ -10493,20 +10493,44 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = (() => {
-  const prizeJourneys = document.querySelector(`.prizes__item--journeys img`);
-  const prizeCases = document.querySelector(`.prizes__item--cases img`);
+  const prizeScreen = document.querySelector(`.screen--prizes`);
+  const prizeJourneys = prizeScreen.querySelector(
+      `.prizes__item--journeys img`
+  );
+  const prizeCases = prizeScreen.querySelector(`.prizes__item--cases img`);
+
+  const images = [
+    {
+      path: `img/prize1.svg`,
+      timeDelay: 0,
+      target: prizeJourneys,
+    },
+    {
+      path: `img/prize2.svg`,
+      timeDelay: 4000,
+      target: prizeCases,
+    },
+  ];
+
+  function addImagesSvg() {
+    if (!prizeJourneys.hasAttribute(`src`)) {
+      images.forEach(({path, timeDelay, target}) => {
+        setTimeout(() => {
+          target.setAttribute(`src`, `${path}?time=${Date.now()}`);
+        }, timeDelay);
+      });
+    }
+    return;
+  }
+
+
+  if (prizeScreen.classList.contains(`active`)) {
+    addImagesSvg();
+  }
 
   document.body.addEventListener(`screenChanged`, (event) => {
     if (event.detail.screenName === `prizes`) {
-
-      // если нет картинки, то добавляем
-      if (!prizeJourneys.hasAttribute(`src`)) {
-        prizeJourneys.setAttribute(`src`, `img/prize1.svg`);
-
-        setTimeout(() => {
-          prizeCases.setAttribute(`src`, `img/prize2.svg`);
-        }, 4000);
-      }
+      addImagesSvg();
     }
   });
 });
@@ -10850,11 +10874,13 @@ Object(_modules_form_js__WEBPACK_IMPORTED_MODULE_6__["default"])();
 Object(_modules_social_js__WEBPACK_IMPORTED_MODULE_7__["default"])();
 Object(_modules_rules__WEBPACK_IMPORTED_MODULE_9__["default"])();
 Object(_modules_intro__WEBPACK_IMPORTED_MODULE_10__["default"])();
-Object(_modules_prizes__WEBPACK_IMPORTED_MODULE_11__["default"])();
 
 const fullPageScroll = new _modules_full_page_scroll__WEBPACK_IMPORTED_MODULE_8__["default"]();
 
-document.addEventListener(`DOMContentLoaded`, () => fullPageScroll.init());
+document.addEventListener(`DOMContentLoaded`, () => {
+  fullPageScroll.init();
+  Object(_modules_prizes__WEBPACK_IMPORTED_MODULE_11__["default"])();
+});
 
 window.addEventListener(`load`, () => {
   document.body.classList.add(`loaded`);
