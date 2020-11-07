2fe63e877f79e792a830b4f7fd42336b9f5be0bc
Victoria <vicczubareva@yandex.ru>
2020-11-07 09:49:24 +0300

    fixed adding images

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,18 +1,42 @@
 export default () => {
-  const prizeJourneys = document.querySelector(`.prizes__item--journeys img`);
-  const prizeCases = document.querySelector(`.prizes__item--cases img`);
+  const prizeScreen = document.querySelector(`.screen--prizes`);
+  const prizeJourneys = prizeScreen.querySelector(
+      `.prizes__item--journeys img`
+  );
+  const prizeCases = prizeScreen.querySelector(`.prizes__item--cases img`);
+
+  const images = [
+    {
+      path: `img/prize1.svg`,
+      timeDelay: 0,
+      target: prizeJourneys,
+    },
+    {
+      path: `img/prize2.svg`,
+      timeDelay: 4000,
+      target: prizeCases,
+    },
+  ];
+
+  function addImagesSvg() {
+    if (!prizeJourneys.hasAttribute(`src`)) {
+      images.forEach(({path, timeDelay, target}) => {
+        setTimeout(() => {
+          target.setAttribute(`src`, `${path}?time=${Date.now()}`);
+        }, timeDelay);
+      });
+    }
+    return;
+  }
+
+
+  if (prizeScreen.classList.contains(`active`)) {
+    addImagesSvg();
+  }
 
   document.body.addEventListener(`screenChanged`, (event) => {
     if (event.detail.screenName === `prizes`) {
-
-      // если нет картинки, то добавляем
-      if (!prizeJourneys.hasAttribute(`src`)) {
-        prizeJourneys.setAttribute(`src`, `img/prize1.svg`);
-
-        setTimeout(() => {
-          prizeCases.setAttribute(`src`, `img/prize2.svg`);
-        }, 4000);
-      }
+      addImagesSvg();
     }
   });
 };
--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,16 @@
 // modules
-import mobileHeight from './modules/mobile-height-adjust.js';
-import slider from './modules/slider.js';
-import menu from './modules/menu.js';
-import footer from './modules/footer.js';
-import chat from './modules/chat.js';
-import result from './modules/result.js';
-import form from './modules/form.js';
-import social from './modules/social.js';
-import FullPageScroll from './modules/full-page-scroll';
-import rules from './modules/rules';
-import intro from './modules/intro';
-import prizes from './modules/prizes';
+import mobileHeight from "./modules/mobile-height-adjust.js";
+import slider from "./modules/slider.js";
+import menu from "./modules/menu.js";
+import footer from "./modules/footer.js";
+import chat from "./modules/chat.js";
+import result from "./modules/result.js";
+import form from "./modules/form.js";
+import social from "./modules/social.js";
+import FullPageScroll from "./modules/full-page-scroll";
+import rules from "./modules/rules";
+import intro from "./modules/intro";
+import prizes from "./modules/prizes";
 
 // init modules
 mobileHeight();
@@ -23,11 +23,13 @@ form();
 social();
 rules();
 intro();
-prizes();
 
 const fullPageScroll = new FullPageScroll();
 
-document.addEventListener(`DOMContentLoaded`, () => fullPageScroll.init());
+document.addEventListener(`DOMContentLoaded`, () => {
+  fullPageScroll.init();
+  prizes();
+});
 
 window.addEventListener(`load`, () => {
   document.body.classList.add(`loaded`);
