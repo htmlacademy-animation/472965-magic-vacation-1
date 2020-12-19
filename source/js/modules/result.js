2dbb5f564e204c4fd40d309eec38319d30aa06b0
Victoria <vicczubareva@yandex.ru>
2020-12-19 12:30:49 +0300

    добавлена svg анимация заголовков

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -5,6 +5,7 @@ export default () => {
     for (let i = 0; i < showResultEls.length; i++) {
       showResultEls[i].addEventListener(`click`, function () {
         let target = showResultEls[i].getAttribute(`data-target`);
+
         [].slice.call(results).forEach(function (el) {
           el.classList.remove(`screen--show`);
           el.classList.add(`screen--hidden`);
@@ -14,6 +15,11 @@ export default () => {
         });
         targetEl[0].classList.add(`screen--show`);
         targetEl[0].classList.remove(`screen--hidden`);
+
+        let titleSvg = targetEl[0].querySelector(`.result__title svg`);
+        let isTitleLost = titleSvg.classList.contains(`result__title_lost`);
+
+        createAnimateSvgTitle(titleSvg, isTitleLost);
       });
     }
 
@@ -28,5 +34,64 @@ export default () => {
         document.getElementById(`message-field`).focus();
       });
     }
+  }
+
+  function createAnimateSvgTitle(title, fall) {
+    const paths = [...title.querySelectorAll(`path`)];
+    let delay = 0.2;
+    let delayTrancform = 0;
+
+    paths.forEach((item, i) => {
+      const pathLength = item.getTotalLength();
+      const itemAnimate = item.querySelector(`animate`);
+
+      item.setAttribute(`stroke-dashoffset`, `0`);
+      item.setAttribute(`stroke-dasharray`, `0 ${pathLength / 2}`);
+
+      itemAnimate.setAttribute(`dur`, `0.4s`);
+      itemAnimate.setAttribute(`to`, `${pathLength / 3} 0`);
+
+      // animateTransform for fall animate
+      if (fall) {
+        const transformList = [
+          `0 0; 0 150; 0 130; 0 150`,
+          `0 0; 0 150; 0 134; 0 150`,
+          `0 0; 0 150; 0 138; 0 150`,
+          `0 0; 0 150; 0 141; 0 150`,
+          `0 0; 0 150; 0 144; 0 150`,
+          `0 0; 0 150; 0 146; 0 150`,
+          `0 0; 0 150; 0 148; 0 150`,
+          `0 0; 0 150; 0 149; 0 150`,
+          `0 0; 0 150; 0 150; 0 150`,
+        ];
+
+        const svgAnimateTransform = document.createElementNS(
+            `http://www.w3.org/2000/svg`,
+            `animateTransform`
+        );
+        svgAnimateTransform.setAttribute(`attributeName`, `transform`);
+        svgAnimateTransform.setAttribute(`type`, `translate`);
+        svgAnimateTransform.setAttribute(
+            `begin`,
+            `animateLetter.begin + ${delayTrancform}s`
+        );
+        svgAnimateTransform.setAttribute(`dur`, `0.8s`);
+        svgAnimateTransform.setAttribute(`values`, `${transformList[i]}`);
+        svgAnimateTransform.setAttribute(
+            `keySplines`, `0 0.6 0.8 1; 0 0.3 0.6 1; 0.3 0 0.6 1`);
+        svgAnimateTransform.setAttribute(`calcMode`, `spline`);
+        svgAnimateTransform.setAttribute(`fill`, `freeze`);
+
+        itemAnimate.setAttribute(`begin`, `${delay}`);
+        delay += 0.13;
+        delayTrancform += 0.15;
+
+        itemAnimate.after(svgAnimateTransform);
+
+        return;
+      }
+
+      itemAnimate.beginElement();
+    });
   }
 };
